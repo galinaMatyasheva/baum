990a6b1c107d3974136d19598c9e4d26b3346aad
galinaMatyasheva <galina_matyasheva@mail.ru>
2021-11-23 08:59:39 +0300

    test 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 
 const App = () => {
-// comment
+// comment 2
   return (
     <div className="app-container">
       <Header/> 
